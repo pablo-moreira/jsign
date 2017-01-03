53d04b547a4aa4aebba41dfae60fbe52ef98f938
205327 <205327@FJ045987-WIN7.tjro.net>
2017-01-03 10:58:42 -0400

    Correção o workarround para alias duplicados estava fazendo dar erro nas versões mais atuais do jre. Foi removido quando usuário possuir certificados com alias duplicados recomendar o mesmo alterar os alias pelo programa do token.

--- src/main/java/com/github/jsign/manager/MSCAPIManager.java
+++ src/main/java/com/github/jsign/manager/MSCAPIManager.java
@@ -23,6 +23,7 @@ import com.github.jsign.keystore.MSCAPIKeyStoreHelper;
 import com.github.jsign.model.Configuration;
 import com.github.jsign.model.MSCAPIAvailableProvider;
 import com.github.jsign.model.OperatingSystem;
+import java.util.Map;
 
 public class MSCAPIManager {
 
@@ -131,7 +132,6 @@ public class MSCAPIManager {
 		try {
 			KeyStore ks = KeyStore.getInstance(TYPE);
 			ks.load(null, null);
-			_fixAliases(ks);
 			return ks;
 		}
 		catch (KeyStoreException e) {
@@ -144,57 +144,7 @@ public class MSCAPIManager {
 			throw new Exception("Erro na comunicação com a MSCAPI!\n" + e);
 		}
 	}
-	
-	/**
-	 * Codigo para resolver o problema de alias iguais para certificados diferentes
-	 * @author http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6672015
-	 * @param keyStore o KeyStore que sera reparado
-	 * @throws Exception 
-	 */
-	@SuppressWarnings("rawtypes")
-	private static void _fixAliases(KeyStore keyStore) throws Exception {
 		
-		Field field;
-		KeyStoreSpi keyStoreVeritable;
-
-		try {
-			field = keyStore.getClass().getDeclaredField("keyStoreSpi");
-			field.setAccessible(true);
-			keyStoreVeritable = (KeyStoreSpi) field.get(keyStore);
-			
-			if ("sun.security.mscapi.KeyStore$MY".equals(keyStoreVeritable.getClass().getName())) {
-				
-				Collection entries;
-				String alias, hashCode;
-				X509Certificate[] certificates;
-
-				field = keyStoreVeritable.getClass().getEnclosingClass().getDeclaredField("entries");
-				field.setAccessible(true);
-				entries = (Collection) field.get(keyStoreVeritable);
-
-				for (Object entry : entries) {
-					field = entry.getClass().getDeclaredField("certChain");
-					field.setAccessible(true);
-					certificates = (X509Certificate[]) field.get(entry);
-
-					hashCode = Integer.toString(certificates[0].hashCode());
-
-					field = entry.getClass().getDeclaredField("alias");
-					field.setAccessible(true);
-					alias = (String) field.get(entry);
-
-					if (!alias.equals(hashCode)) {
-						field.set(entry, alias.concat(" - ").concat(hashCode));
-					} 
-				}
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			throw new Exception(MessageFormat.format("Erro ao realizar o unificação dos alias dos certificados da MsCAPI, {0}", e.getMessage()));
-		}
-	}
-	
 	public void initMscapiKeyStore(MSCAPIKeyStoreHelper keyStoreHelper) throws Exception {
 
 		try {			
